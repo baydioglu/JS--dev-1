8526da0c898a71fde3b64af01af52fcaeba99c06
Your Name <you@example.com>
2023-01-17 12:44:54 +0300

    Date Added

--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,13 @@ userName = prompt("Please enter your name.");
 document.querySelector("#myName").innerHTML += `${userName}`;
 
 let today = new Date();
+let dateNow = today.getDate();
+let monthNow = today.getMonth();
+let yearNow = today.getFullYear();
 let hourNow = today.getHours();
 let minutesNow = today.getMinutes();
 let secondsNow = today.getSeconds();
 
 document.querySelector("#myClock").innerHTML += `${hourNow}:${minutesNow}:${secondsNow}`;
+
+document.querySelector("#myDate").innerHTML += `${dateNow}/${monthNow}/${yearNow}`;
